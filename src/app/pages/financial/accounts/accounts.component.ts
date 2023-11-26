68fb0e6f3f262188037030b0c28414a2159fbe90
Luis Sarmiento <lsarmientom010@gmail.com>
2023-11-26 17:46:06 -0500

    Add budget details to accounts component

--- src/app/pages/financial/accounts/accounts.component.ts
+++ src/app/pages/financial/accounts/accounts.component.ts
@@ -1,11 +1,58 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, CurrencyPipe } from '@angular/common';
+import { TooltipDirective } from 'tooltip';
 
 @Component({
   selector: 'app-accounts',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, CurrencyPipe, TooltipDirective],
   templateUrl: './accounts.component.html',
   styles: ``,
 })
-export default class AccountsComponent {}
+export default class AccountsComponent {
+  budgets = [
+    {
+      budget: 'Home',
+      summarize:
+        'Home, utilities, rent/mortgage, home maintenance, groceries, pet',
+      balance: 10000,
+      spent: 5000,
+      remaining: 5000,
+    },
+    {
+      budget: 'Hobbies',
+      summarize: 'hobbies, dinning out, Entertainment, cine, coffee',
+      balance: 10000,
+      spent: 5000,
+      remaining: 5000,
+    },
+    {
+      budget: 'Health',
+      summarize: 'Health, doctor, dentist, pharmacy, gym',
+      balance: 10000,
+      spent: 5000,
+      remaining: 5000,
+    },
+    {
+      budget: 'Transportation',
+      summarize: 'Transportation, car payment, gas, insurance, maintenance',
+      balance: 10000,
+      spent: 5000,
+      remaining: 5000,
+    },
+    {
+      budget: 'Services',
+      summarize: 'Electricity, water, gas, phone, internet, subscriptions',
+      balance: 10000,
+      spent: 5000,
+      remaining: 5000,
+    },
+    {
+      budget: 'Other',
+      summarize: 'Other, fees, taxes, hair, gift, savings, investments',
+      balance: 10000,
+      spent: 5000,
+      remaining: 5000,
+    },
+  ];
+}
